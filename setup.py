198cd1de99335fb3eb4eba50ffa1f958dc17429d
Faizan Siddiqi <51734705+Faizan272@users.noreply.github.com>
2022-11-25 19:26:44 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'Faizan_TwoMachine',
+  packages = ['Faizan_TwoMachine'],
+  version = '0.1',
+  license='MIT',
+  description = 'This Johnson Scheduling Algorithm works on Data Frames',
+  author = 'FAIZAN SIDDIQI',
+  author_email = 'faizansiddiqi27@gmail.com', 
+  url = 'https://github.com/Faizan272/Faizan_TwoMachine',
+  download_url = 'https://github.com/Faizan272/Faizan_two_machines/archive/refs/tags/0.1.tar.gz',
+  install_requires=[
+        'pandas','tabulate'
+      ])
